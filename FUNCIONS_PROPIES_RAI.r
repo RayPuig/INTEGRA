0c0568cc9f3f188e692027c0b4614c7fda9c70b9
Ramon Puig Treserres <38122893W@pbcn.cpd1.grupics.intranet>
2019-09-26 12:11:00 +0200

    Afegides funcions propies i conductors

---
+++ FUNCIONS_PROPIES_RAI.r
@@ -0,0 +1,205 @@
+##################
+####[29.5.2019]###
+##[FUNCIONS RAI]##
+##################
+
+
+#------------------------------------------------------------------------------------------#
+#                        1. ETIQUETAR VARIABLES! []
+#------------------------------------------------------------------------------------------#
+#------------------------------------------------------------------#
+etiquetar<-function(d=dadestotal,taulavariables="variables_R.xls") {
+  
+  ####  Llegir etiquetes i variables a analitzar ####
+  variables <- readxl::read_excel(taulavariables)
+  variables[is.na(variables)]<- 0
+  #
+  #
+  ###################################   etiquetar variables         
+  seleccio<-variables
+  camp<- as.vector(seleccio$camp) #
+  descripcio<- as.vector(seleccio$descripcio) #
+  ### etiquetar variables seleccionades     #
+  for (i in 2:length(descripcio)){if (any(colnames(d) == camp[i])) {Hmisc::label(d[[camp[i]]]) <- descripcio[i]}}
+  d
+}
+#------------------------------------------------------------------#
+#------------------------------------------------------------------#
+
+
+
+convertir_dates<-function(d=dadestotal,taulavariables="variables_R.xls")
+  
+{
+  ####  Llegir etiquetes i variables a analitzar ####
+  variables <- readxl::read_excel(taulavariables)
+  variables[is.na(variables)]<- 0
+  #
+  #
+  ###################################   etiquetar variables         
+  seleccio<-variables
+  camp<- as.vector(seleccio$camp) #
+  # dates<-as.vector(seleccio$dates)
+  # dates<-seleccio[[campdata]]
+  ### etiquetar variables seleccionades     #
+  
+  for (i in 1:length(camp)){if (seleccio$dates[i]==1) { 
+    
+    pepito<-paste0("as.Date(d[[camp[",i,"]]], '%Y%d%m')")
+    
+    d[[camp[i]]]<-eval(parse(text=pepito))
+    
+  } }
+  
+  d
+  
+}
+
+
+
+#------------------------------------------------------------------#
+#------------------------------------------------------------------#
+LAB_ETIQ<-function(dt=dades,variables_factors=conductor_variables,fulla="etiquetes",idioma="etiqueta1"){
+  
+  #dt=dades
+  #variables_factors=conductor_variables
+  #fulla="etiquetes"
+  #idioma="etiqueta2"
+  
+  
+  variables_factors<-readxl::read_excel(variables_factors,sheet=fulla)
+  
+  if (idioma=="etiqueta1") {
+    
+    k<-variables_factors%>%select(camp, valor,etiqueta1)
+    
+  } else if (idioma=="etiqueta2") {
+    
+    k<-variables_factors%>%select(camp, valor,etiqueta2)
+    k<-k%>%mutate(etiqueta1=etiqueta2)
+    k<-k%>%select(camp, valor,etiqueta1)
+    
+  }
+  
+  k2<-k%>%spread(key=valor,value=etiqueta1)
+  
+  noms_variables<-(k2$camp)
+  num_vars<-length(k2$camp)
+  
+  
+  pepito<-k%>% split(list(.$camp))
+  
+  for (i in 1:num_vars) {
+    
+    dt[noms_variables[i]]<-lapply(dt[noms_variables[i]],function(y) factor(y,levels=pepito[[i]]$valor,labels=pepito[[i]]$etiqueta1))
+  }
+  
+  dt}
+#------------------------------------------------------------------#
+
+
+
+#------------------------------------------------------------------#
+LAB_ETIQ_v2<-function(dt=dades,variables_factors=conductor_variables,fulla="etiquetes",idioma="etiqueta1"){
+  
+  #dt=DT_FINAL
+  #variables_factors=conductor_variables
+  #fulla="etiquetes"
+  #idioma="etiqueta1"
+  
+  
+  #------------------------------------------------------------------#
+  variables_factors<-readxl::read_excel(variables_factors,sheet=fulla)
+  #------------------------------------------------------------------#
+  
+  
+  
+  if (idioma=="etiqueta1") {
+    
+    k<-variables_factors%>%dplyr::select(camp, valor,etiqueta1)
+    
+  } else if (idioma=="etiqueta2") {
+    
+    k<-variables_factors%>%dplyr::select(camp, valor,etiqueta2)
+    k<-k%>%mutate(etiqueta1=etiqueta2)
+    k<-k%>%dplyr::select(camp, valor,etiqueta1)
+    
+  }
+  
+  
+  #------------------------------------------------------------------#
+  pepe<-k %>% split(list(.$camp))
+  #------------------------------------------------------------------#
+  #
+  noms_variables<-names(pepe)
+  num_vars<-length(noms_variables)
+  
+  #dt[noms_variables[1]]
+  
+  for (i in 1:num_vars) {
+    
+    
+    dt[noms_variables[i]]<-lapply(dt[noms_variables[i]],function(y) factor(y,levels=pepe[[i]]$valor,labels=pepe[[i]]$etiqueta1))
+    
+    
+  }
+  
+  
+  
+  
+  dt}
+#------------------------------------------------------------------#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####   FORMULA MILLORADA --------------------------
+#
+# Te en compte l'Ordre que està posada en el conductor taulavariables
+#
+#
+formula_compare=function(x="taula1",y="grup",elimina=c("IDP"),taulavariables="variables_R.xls") {
+  
+  # x="T1_GSK"
+  # y="."
+  # taulavariables ="VARIABLES.xls"
+  # elimina=c("IDP")
+  
+  
+  # 1. Llegir conductor analisis 
+  
+  variables <- readxl::read_excel(taulavariables)
+  variables[is.na(variables)]<- 0
+  
+  # 2. DATA table filtrar ordenar llista de camps
+  polio<-data.table::data.table(variables)
+  
+  mua<-polio[camp!=elimina] %>% 
+    dplyr::filter_(paste0(x,">0")) %>% 
+    dplyr::arrange_(x) %>% 
+    dplyr::select(camp) %>% as.vector()
+  
+  # 3. Generar formula
+  
+  # y<-as.formula(paste(y, paste(llista$camp, collapse=" + "), sep=" ~ "))
+  
+  y<-as.formula(paste(y, paste(mua$camp, collapse=" + "), sep=" ~ "))
+  
+  y
+  
+}
+
